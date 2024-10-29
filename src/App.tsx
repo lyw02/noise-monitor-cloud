668bb2b43d52e13989f70482912e4cd3e4b76c6d
Yiwei Liu <2814824250@qq.com>
2024-10-29 11:50:15 +0000

    feat: amplify auth

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import NoiseDisplayer from "./components/NoiseDisplayer";
 
 import { Amplify } from 'aws-amplify';
 import type { WithAuthenticatorProps } from '@aws-amplify/ui-react';
-import { withAuthenticator } from '@aws-amplify/ui-react';
+import { withAuthenticator, useAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 import config from './amplifyconfiguration.json';
 Amplify.configure(config);
@@ -12,9 +12,12 @@ Amplify.configure(config);
 export function App() {
   const [isMonitoring, setIsMonitoring] = useState(false);
 
+  const { user, signOut } = useAuthenticator((context) => [context.user]);
+
   return (
     <>
       <h1>Noise Monitor</h1>
+      <span>{user.signInDetails?.loginId}</span>
       <div className="card">
         <button onClick={() => setIsMonitoring((prev) => !prev)}>
           {isMonitoring ? "End monitoring" : "Start monitoring"}
